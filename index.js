03fd270679960e1f3d195a55bd2d42a7c52b0005
hersson ceballos <herssonceballos@gmail.com>
2020-10-25 16:18:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var a, b
+
+a= 2 
+b= 4 
+
+ 
+function add() {
+  a += 5
+}
+
+function subtract() {
+  a-= 5
+}
+
+function multiply() {
+  a*= 5
+}
+
+function divide() {
+  a/= 5
+}
+
+function add() {
+ a + b
+}
